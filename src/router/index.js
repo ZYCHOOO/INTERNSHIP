61d284f0237778eea1f4d7069f79dd332727fcb2
ZYCHOOO <931187106@qq.com>
2020-05-20 10:47:00 +0800

    update: 初始化项目/router配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/views/Index'
+// 基础页面
+import Main from '@/views/Main'
+import Male from '@/views/Male'
+import Female from '@/views/Female'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path:'/',name:'Index',component:Index,meta:{title:'起始页面'}},
+    // 基础页面
+    {path:'/Main',name:'Main',component:Main,meta:{title:'首页'}},
+    {path:'/Male',name:'Male',component:Male,meta:{title:'男士'}},
+    {path:'/Female',name:'Female',component:Female,meta:{title:'女士'}}
   ]
 })
