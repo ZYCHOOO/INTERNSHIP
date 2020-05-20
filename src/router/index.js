fbe34970cafefdfe2cb7b2b367c3fbf86a928ec3
ZYCHOOO <931187106@qq.com>
2020-05-20 11:50:12 +0800

    update: eslint格式化/添加照片

--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,10 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:'/',name:'Index',component:Index,meta:{title:'起始页面'}},
+    { path: '/', name: 'Index', component: Index, meta: { title: '起始页面' }},
     // 基础页面
-    {path:'/Main',name:'Main',component:Main,meta:{title:'首页'}},
-    {path:'/Male',name:'Male',component:Male,meta:{title:'男士'}},
-    {path:'/Female',name:'Female',component:Female,meta:{title:'女士'}}
+    { path: '/Main', name: 'Main', component: Main, meta: { title: '首页' }},
+    { path: '/Male', name: 'Male', component: Male, meta: { title: '男士' }},
+    { path: '/Female', name: 'Female', component: Female, meta: { title: '女士' }}
   ]
 })
